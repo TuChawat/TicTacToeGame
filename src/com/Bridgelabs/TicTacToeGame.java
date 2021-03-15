5714f261c09bbb102b294e09d69f9211902e9baa
TuChawat <76853681+TuChawat@users.noreply.github.com>
2021-03-15 13:49:38 +0530

    Update TicTacToeGame.java

--- src/com/Bridgelabs/TicTacToeGame.java
+++ src/com/Bridgelabs/TicTacToeGame.java
@@ -1,9 +1,14 @@
 package com.Bridgelabs;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
     public static void main(String[] args) {
         System.out.println("Welcome to Game");
         char[] board = createBoard();
+        Scanner userInput = new Scanner(System.in);
+        char userLetter = chooseUserLetter(userInput);
+        char computerLetter = (userLetter == 'X') ? 'O' : 'X';
     }
 
     private static char[] createBoard() {
@@ -13,8 +18,11 @@ public class TicTacToeGame {
         }
         return board;
     }
+    public static char chooseUserLetter(Scanner userInput) {
+        System.out.print("Choose your Letter: ");
+        return userInput.next().toUpperCase().charAt(0);
+    }
 }
-
 
 
 
